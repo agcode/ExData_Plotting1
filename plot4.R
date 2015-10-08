97cca7da9a6362fcc8c7ea7416361aa65ae9f4e0
agcode <cheri.geor@gmail.com>
2015-10-08 12:56:54 +0530

    plot4.R

---
+++ plot4.R
@@ -0,0 +1,25 @@
+## The unzipped file 
+file<-"household_power_consumption.txt"
+## Read data into a table
+x<-read.table(file,na.strings="?",skip=1,col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),sep=";")
+## Convert Date and time appropriately 
+x$Date<-as.Date(x$Date,format="%d/%m/%Y")
+x$Time<-strftime(strptime(x$Time,format="%H:%M:%S"),"%H:%M:%S")
+## Subset only the data with dates ranging between 2007-02-01 and 2007-02-02
+y<-subset(x,Date >="2007-02-01" & Date <= "2007-02-02")
+## Generate the plot of the histogram and save as a png
+png("plot4.png",width= 480,height= 480,units= "px")
+par(mfrow=c(2,2))
+## Plot 1 
+plot(as.POSIXct(paste(y$Date, y$Time), format="%Y-%m-%d %H:%M:%S"),y$Global_active_power,type="l",ylab="Global Active Power",xlab="",main="")
+## Plot 2
+plot(as.POSIXct(paste(y$Date, y$Time), format="%Y-%m-%d %H:%M:%S"),y$Voltage,type="l",ylab="Voltage",xlab="datetime",main="")
+## Plot 3
+plot(as.POSIXct(paste(y$Date, y$Time), format="%Y-%m-%d %H:%M:%S"),y$Sub_metering_1,type="n",ylab="Energy Sub Metering",xlab="",main="")
+lines(as.POSIXct(paste(y$Date, y$Time), format="%Y-%m-%d %H:%M:%S"),y$Sub_metering_1,type="l",col="black")
+lines(as.POSIXct(paste(y$Date, y$Time), format="%Y-%m-%d %H:%M:%S"),y$Sub_metering_2,type="l",col="red")
+lines(as.POSIXct(paste(y$Date, y$Time), format="%Y-%m-%d %H:%M:%S"),y$Sub_metering_3,type="l",col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lwd=3)
+## Plot 4
+plot(as.POSIXct(paste(y$Date, y$Time), format="%Y-%m-%d %H:%M:%S"),y$Global_reactive_power,type="l",ylab="Global_Reactive_Power",xlab="datetime",main="")
+dev.off()
